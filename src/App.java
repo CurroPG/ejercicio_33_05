87171add3d634c5d233b252bc9feeb3862395772
Curro Portillo <211curroportillo@gmail.com>
2025-11-03 18:04:42 +0100

    ejercicio 33

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la U: "));
+        int longitudLinea = 1;
+        int base = 1;
+        while(base<altura){
+            for(int i = 1; i<=altura; i++){
+                if(i == 1 || i == altura){
+                    System.out.print("* ");
+                    longitudLinea++;
+                }else
+                        System.out.print("  ");
+            }
+            System.out.println();
+            base++;
+        }
+        for(int i = 1; i<=longitudLinea; i++){
+            if(i%2 == 0 || i == 1 || i == longitudLinea)
+                System.out.print(" ");
+                else
+                    System.out.print("*");
+        }
     }
 }
