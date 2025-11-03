c00f2f9f5bd519cafeb4b1b6ca2c8b8efc0e86ef
Curro Portillo <211curroportillo@gmail.com>
2025-11-03 18:09:20 +0100

    ejercicio 33 corregido

--- src/App.java
+++ src/App.java
@@ -3,8 +3,13 @@ public class App {
         int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la U: "));
         int longitudLinea = 1;
         int base = 1;
+        System.out.println();
         while(base<altura){
-            for(int i = 1; i<=altura; i++){
+            for(int i = 0; i<=altura; i++){
+                if(i == 0){
+                    System.out.print(" ");
+                    i++;
+                }
                 if(i == 1 || i == altura){
                     System.out.print("* ");
                     longitudLinea++;
@@ -14,7 +19,11 @@ public class App {
             System.out.println();
             base++;
         }
-        for(int i = 1; i<=longitudLinea; i++){
+        for(int i = 0; i<=longitudLinea; i++){
+            if(i == 0){
+                System.out.print(" ");
+                i++;
+            }
             if(i%2 == 0 || i == 1 || i == longitudLinea)
                 System.out.print(" ");
                 else
